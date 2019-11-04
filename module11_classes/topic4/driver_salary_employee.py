61308587847b2c1b8efeb9823e4aa038935e746c
Kellymsmith7 <kellymsmith7@gmail.com>
2019-11-04 17:54:21 -0600

    Employee & Salary Class and Driver Try 3

--- module11_classes/topic4/driver_salary_employee.py
+++ module11_classes/topic4/driver_salary_employee.py
@@ -19,6 +19,6 @@ from module11_classes.topic4.salaried_employee_class import SalariedEmployee
 new_emp = Employee('Smith', 'Kelly', '5151234567', '123 Main St', 'Des Moines, IA')
 new_emp_salaried = SalariedEmployee(new_emp.last_name, new_emp.first_name, new_emp.phone_number, new_emp.address_1, new_emp.address_2, 40000, '10-20-2019')
 print(new_emp_salaried.display())
-new_emp_salaried.give_raise(42000)
+new_emp_salaried.give_raise(45000)
 print(new_emp_salaried.display())
 del new_emp
